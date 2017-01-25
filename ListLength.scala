b99c2bb0f048e59c9efed2b44a4a734f838f0a72
anuj1207 <anuj.saxena@knoldus.in>
2017-01-25 11:16:43 +0530

    Update ListLength.scala
    Final Submission :: Added hasSubsequence Method

--- ListLength.scala
+++ ListLength.scala
@@ -13,6 +13,16 @@ package com.knoldus.list.detail.assignment
 
     }
 
+    def hasSubsequence[A](list: List[A], sub: List[A]): Boolean = {
+
+      val listLength = list.length
+      val subLength = sub.length
+      val upperLimit = listLength - subLength
+      val resultList = for (i <- 0 to upperLimit) yield list.slice(i, subLength) == sub
+      resultList.nonEmpty
+
+    }
+	  
     def concateList[A](l1:List[A],l2:List[A]):List[A] = {
       
       l2 match {
@@ -44,15 +54,18 @@ package com.knoldus.list.detail.assignment
       val list = List(1,2,3,4,5)
       val listMethods = new ListMethods()
 	
-	  println("\n Getting Length Method \n")	      
+      println("\n Getting Length Method \n")	      
       val listLength = listMethods.length(list)
       println("Length: "+listLength)
 
+      println("\n Checking Subsequence: ")
+      println(listMethods.hasSubsequence(List(1, 2, 3, 4, 5), List(4, 5)))
+	    
       println("\n ConcateList Method \n")
       println(listMethods.concateList(List(1, 2, 3), List(5, 6, 4)))
      
- 	  println("\n SplitList Method \n")
- 	  println(listMethods.splitList(List(1,2,3,4,5,6),(x:Int) => x>2)) 
+      println("\n SplitList Method \n")
+      println(listMethods.splitList(List(1,2,3,4,5,6),(x:Int) => x>2)) 
     }
 
   }
